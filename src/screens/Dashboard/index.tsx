5e42ae3d7744c06400a81aab68edf9f4d557e17f
Matheus Aguiar <first_mf@hotmail.com>
2021-08-04 20:12:18 -0300

    add: Header component

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 
-import * as S from './styles';
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  Username,
+  Icon,
+} from './styles';
 
 export function Dashboard() {
   return (
-    <S.Container>
-      <S.Title>Dashboard</S.Title>
-    </S.Container>
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/31289353?v=4',
+              }}
+            />
+            <User>
+              <UserGreeting>Ola, </UserGreeting>
+              <Username>Matheus</Username>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+    </Container>
   );
 }
